4fff6c6134c32dbec397a9a23f718ce2d2abdb8b
sebneedles <sebneedles@gmail.com>
2022-12-01 17:38:09 +0100

    Add Components, static datas, cartItems counter

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Header = (props) => {
+  const { countCartItems } = props;
+
+  return (
+    <header className="row block center">
+      <div>
+        <a href="/">
+          <h1>Mini Commerce</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">
+          Cart{' '}
+          {countCartItems ? (
+            <button className="badge">{countCartItems}</button>
+          ) : (
+            ''
+          )}
+        </a>{' '}
+        <a href="#/signin">SignIn</a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'MacBook',
+      price: 1400,
+      image: 'https://picsum.photos/id/180/2400/1600',
+    },
+    {
+      id: '2',
+      name: 'Ipad',
+      price: 800,
+      image: 'https://picsum.photos/id/181/2400/1600',
+    },
+    {
+      id: '3',
+      name: 'Imac',
+      price: 1800,
+      image: 'https://picsum.photos/id/182/2400/1600',
+    },
+  ],
+};
+
+export default data;
